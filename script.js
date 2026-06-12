172cf1fd1dc62bb235c03bd5fb0888df27eadf8b
efeglr <257828430+efeglr@users.noreply.github.com>
2026-06-12 10:29:10 +0200

    Revisiting Rock Paper Scissors

--- script.js
+++ script.js
@@ -1,66 +1,83 @@
-//a method that chooses randomly for the Computer
-function getComputerChoice(){
-    let randomNum = Math.random();
-    if (randomNum <= 0.333333){
-        return("Rock");
-    }
-    else if (randomNum >= 0.666666){
-        return("Paper");
-    }
-    else{
-        return("Scissors");
-    }
-}
-
-//the logic to get the human choice
-function getHumanChoice(){
-    let input = prompt("rock, paper or scissors?");
-    switch(input){
-        case "rock":
-            return "rock";
-        case "paper":
-            return "paper";
-        case "scissors":
-            return "scissors";
-    }
-}
-
-
-function playGame(){
-    //Declare the players score
+    const rock_button = document.querySelector("#rock");
+    const paper_button = document.querySelector("#paper");
+    const scissors_button = document.querySelector("#scissors");
+    rock_button.addEventListener("click",()=>playRound("SCISSORS",getComputerChoice()));
+    paper_button.addEventListener("click",()=>playRound("PAPER",getComputerChoice()));
+    scissors_button.addEventListener("click",()=>playRound("SCISSORS",getComputerChoice()));
     let humanScore = 0;
     let computerScore = 0;
-    
+    let score = document.createElement("div");
+    document.body.append(score);
+
+
+    //a method that chooses randomly for the Computer
+    function getComputerChoice(){
+        let randomNum = Math.random();
+        if (randomNum <= 1/3){
+            return("ROCK");
+        }
+        else if (randomNum >= 2/3){
+            return("PAPER");
+        }
+        else{
+            return("SCISSORS");
+        }
+    }
+
+    //the logic to get the human choice
+    /* function getHumanChoice(){
+        let input = prompt("rock, paper or scissors?");
+        switch(input){
+            case "rock":
+                return "rock";
+            case "paper":
+                return "paper";
+            case "scissors":
+                return "scissors";
+        }
+    }
+    */
+    function checkWinner(){
+        if (computerScore === 5 || humanScore === 5){
+            if(computerScore===5){
+                alert("The computer won, because it reached 5!");
+                computerScore = 0;
+                humanScore = 0;
+                // clearing the scores inside of the div
+                while(score.firstChild) score.firstChild.remove();
+            }
+            if(humanScore===5){
+                alert("You won, because you have reached 5!");
+                computerScore = 0;
+                humanScore = 0;
+                // clearing the scores inside of the div
+                while(score.firstChild) score.firstChild.remove();
+            }
+        }
+    }
+
+    function showScore(){
+        score.innerHTML = "Your score is " +humanScore + "<br>" + "Computers score is " +computerScore;
+    }
+
     //The logic behind a round being played
     function playRound(humanChoice, computerChoice){
-        let humanChoiceUpperCase = humanChoice.toUpperCase();
-        let computerChoiceUpperCase = computerChoice.toUpperCase();
-    
-        switch(humanChoiceUpperCase){
+
+        switch(humanChoice){
             case "ROCK":
-                (computerChoiceUpperCase==="PAPER")? (alert("You lose!"),computerScore++): 
-                (computerChoiceUpperCase==="SCISSORS")? (alert("You win!"),humanScore++): 
+                (computerChoice==="PAPER")? (alert("You lose!"),++computerScore,showScore(),checkWinner()): 
+                (computerChoice==="SCISSORS")? (alert("You win!"),++humanScore,showScore(),checkWinner()):
                 alert("It's a tie!");
-            break;
+                break;
             case "PAPER":
-                (computerChoiceUpperCase==="SCISSORS")? (alert("You lose"), computerScore++) : 
-                (computerChoiceUpperCase==="ROCK")?  (alert("You Win!"), humanScore++) :
+                (computerChoice==="SCISSORS")? (alert("You lose"), ++computerScore,showScore(),checkWinner()): 
+                (computerChoice==="ROCK")?  (alert("You Win!"), ++humanScore,showScore(),checkWinner()) :
                 alert("It's a tie!");
-            break;
+                break;
             case "SCISSORS":
-                (computerChoiceUpperCase==="PAPER")? (alert("You Win!"),humanScore++) : 
-                (computerChoiceUpperCase==="ROCK")? (alert("You Lose!"),computerScore++) : 
+                (computerChoice==="PAPER")? (alert("You Win!"),++humanScore,showScore(),checkWinner()) : 
+                (computerChoice==="ROCK")? (alert("You Lose!"),++computerScore,showScore(),checkWinner()) :
                 alert("It's a tie!");
-            break;
+                break;
         }
-    } 
-
-    //5 rounds are being played
-    playRound(getHumanChoice(),getComputerChoice());
-    playRound(getHumanChoice(),getComputerChoice());
-    playRound(getHumanChoice(),getComputerChoice());
-    playRound(getHumanChoice(),getComputerChoice());
-    playRound(getHumanChoice(),getComputerChoice());
-}
-
-playGame();
+    }
